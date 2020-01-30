62e437694c273c0d2a375ac34504fe74b97b8aa3
Fer Torres <fertorresmx@gmail.com>
2020-01-30 03:07:00 -0600

    Add layout view, index view, new route and Image model

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'laravel-crud-image-gallery'], function () {
+    Route::get('/', 'Crud4Controller@index');
+    Route::match(['get', 'post'], 'create', 'Crud4Controller@create');
+    Route::match(['get', 'put'], 'update/{id}', 'Crud4Controller@update');
+    Route::delete('delete/{id}', 'Crud4Controller@delete');
+});
